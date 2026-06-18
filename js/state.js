840298fe5c859a71dd1ebde90d8d6e7f18156d24
cosmicvoyager010 <cosmicvoyager010@gmail.com>
2026-06-18 22:58:49 +0900

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,13 +1,18 @@
-export const APP_NAME = "PawTrail";
-
 export const state = {
-  worldMood: "CALM",
-  lastMemoryAt: null,
+  world: {
+    totalKm: 0,
+    zones: {
+      home: { unlocked: true },
+      park: { unlocked: false },
+      forest: { unlocked: false },
+      river: { unlocked: false }
+    }
+  },
+
   puppy: {
     mood: "RESTING",
-    bond: 0.3,
-    energy: 0.7,
-    trust: 0.5,
-    playfulness: 0.4
-  }
+    bond: 0
+  },
+
+  memory: []
 };
